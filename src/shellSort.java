9f672e0e1c377d2f74f5b30cb57c4b3237281138
hmj <hu782980856@gmail.com>
2020-06-20 14:42:15 +0800

    希尔排序

--- src/shellSort.java
+++ src/shellSort.java
@@ -1,15 +1,13 @@
-package guiBingSort;
+public class shellSort {
 
-public class guiBingSort {
-
-    // 归并排序
-    public static void guiBingSort(int[] arrays) {
+    // 希尔排序-就是升级版的插入排序
+    public static void shellSort(int[] arrays) {
         if (arrays == null || arrays.length < 1) {
             return;
         }
         for (int gap = arrays.length / 2; gap > 0; gap = gap / 2) {
             for (int i = gap; i < arrays.length; i = i + gap) {
-                for (int j = gap; j >= 0; j = j - gap) {
+                for (int j = i; j > 0; j = j - gap) {
                     if (arrays[j] < arrays[j - gap]) {
                         int temp = arrays[j];
                         arrays[j] = arrays[j - gap];
@@ -18,6 +16,14 @@ public class guiBingSort {
                 }
             }
         }
-        System.out.println("");
+        System.out.println("希尔排序完成");
+        for (Integer i : arrays) {
+            System.out.print(i);
+            System.out.print(" ");
+        }
+    }
+
+    public static void main(String[] args) {
+        shellSort(new int[]{4, 52, 5, 2, 4, 23, 523, 523, 4, 5});
     }
 }
