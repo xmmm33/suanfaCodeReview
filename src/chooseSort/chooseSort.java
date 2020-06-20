74a41db46a7f7342a15790c5cb6ec3da9e5374ac
hmj <hu782980856@gmail.com>
2020-06-20 14:29:41 +0800

    选择排序

--- src/chooseSort/chooseSort.java
+++ src/chooseSort/chooseSort.java
@@ -10,6 +10,27 @@ public class chooseSort {
         if (arrays == null || arrays.length < 1) {
             return;
         }
-
+        for (int i = 0; i < arrays.length - 1; i++) {
+            int minindex = i;
+            for (int j = i + 1; j < arrays.length; j++) {
+                if (arrays[minindex] > arrays[j]) {
+                    minindex = j;
+                }
+            }
+            int temp = arrays[minindex];
+            arrays[minindex] = arrays[i];
+            arrays[i] = temp;
+        }
+        System.out.println("选择排序完成");
+        for (Integer i : arrays) {
+            System.out.print(i);
+            System.out.print(" ");
+        }
     }
+
+    public static void main(String[] args) {
+        chooseSort(new int[]{4,52,5,2,4,23,523,523,4,5});
+    }
+
+
 }
