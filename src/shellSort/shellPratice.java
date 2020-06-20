f00bb31ed630f81e266f6665f211745aa2bb07c6
hmj <hu782980856@gmail.com>
2020-06-20 15:35:14 +0800

    希尔排序练习

---
+++ src/shellSort/shellPratice.java
@@ -0,0 +1,28 @@
+package shellSort;
+
+public class shellPratice {
+
+    public static void main(String[] args) {
+        shellPractice(new int[]{5, 34, 56, 6, 5, 6, 1, 24, 4});
+    }
+
+    public static void shellPractice(int[] arrays) {
+        if (arrays == null || arrays.length < 1) {
+            return;
+        }
+        for (int gap = arrays.length / 2; gap > 0; gap = gap / 2) {
+            for (int i = gap; i < arrays.length; i = i + gap) {
+                for (int j = i; j > 0; j = j - gap) {
+                    if (arrays[j] < arrays[j - gap]) {
+                        int temp = arrays[j];
+                        arrays[j] = arrays[j - gap];
+                        arrays[j - gap] = temp;
+                    }
+                }
+            }
+        }
+        for (Integer i : arrays) {
+            System.out.println(i);
+        }
+    }
+}
