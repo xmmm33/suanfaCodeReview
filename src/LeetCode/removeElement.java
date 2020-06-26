8c19e1ab87814209e90ad4ba94f0248b7188105b
hmj <hu782980856@gmail.com>
2020-06-26 13:57:23 +0800

    移除元素，跟上一题是一样的思想和方法

---
+++ src/LeetCode/removeElement.java
@@ -0,0 +1,32 @@
+package LeetCode;
+
+public class removeElement {
+
+    /**
+     * 移除所有等于val的元素，要求就地修改
+     * 双指针
+     * @param nums
+     * @param val
+     * @return
+     */
+    public static int removeElement(int[] nums, int val) {
+        if (nums == null || nums.length < 1) {
+            return 0;
+        }
+        int i = 0;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != val) {
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+        return i;
+    }
+
+
+    public static void main(String[] args) {
+//        removeElement(new int[]{0, 1, 2, 2, 3, 0, 4, 2}, 2);
+        removeElement(new int[]{4,5}, 5);
+//        removeElement(new int[]{3}, 2);
+    }
+}
