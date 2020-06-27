1cca3bedafce9aa9ad1cec2588bceeadb714c473
hmj <hu782980856@gmail.com>
2020-06-27 15:27:13 +0800

    最大连续子数组和,简单的动态规划

---
+++ src/LeetCode/Arrays/DP/maxSubArray.java
@@ -0,0 +1,30 @@
+package LeetCode.Arrays.DP;
+
+public class maxSubArray {
+
+    /**
+     * 最大子数组和
+     * 输入: [-2,1,-3,4,-1,2,1,-5,4],
+     * 输出: 6
+     * 解释: 连续子数组 [4,-1,2,1] 的和最大，为 6。
+     * @param nums
+     * @return
+     */
+    public static int maxSubArray(int[] nums) {
+        if (nums == null || nums.length < 1) {
+            return 0;
+        }
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        int maxSum = dp[0];
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
+            maxSum = Math.max(maxSum, dp[i]);
+        }
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+        maxSubArray(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4});
+    }
+}
