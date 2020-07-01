1306cdd964a4cca3749a40562c605337aa8aaa6d
hmj <hu782980856@gmail.com>
2020-07-01 13:43:35 +0800

    根据中序和后序重建二叉树，思路就是按照后序的最后一个元素来划分中序序列，然后递归

---
+++ src/LeetCode/BinTree/buildTreeII.java
@@ -0,0 +1,38 @@
+package LeetCode.BinTree;
+
+import java.util.Arrays;
+
+public class buildTreeII {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+
+    /**
+     * 从中序和后续重建二叉树
+     * @param inorder
+     * @param postorder
+     * @return
+     */
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        if (inorder.length < 1 || postorder.length < 1) {
+            return null;
+        }
+        TreeNode root = new TreeNode(postorder[postorder.length - 1]);
+        for (int i = 0; i < inorder.length; i++) {
+            if (inorder[i] == postorder[postorder.length - 1]) {
+                root.left = buildTree(Arrays.copyOfRange(inorder, 0, i), Arrays.copyOfRange(postorder, 0,i));
+                root.right = buildTree(Arrays.copyOfRange(inorder, i + 1, inorder.length), Arrays.copyOfRange(postorder, i, postorder.length - 1));
+                break;
+            }
+        }
+        return root;
+    }
+}
