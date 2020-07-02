81de7994a85b0be02f84892ef7a3025e97c83f89
xmmm <782980856@qq.com>
2020-07-02 23:13:49 +0800

    三角矩阵最短路径，使用的是自底向上的动态规划，也可以从上往下的DP，注意总结和复习

---
+++ src/LeetCode/Arrays/minimumTotal.java
@@ -0,0 +1,29 @@
+package LeetCode.Arrays;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class minimumTotal {
+
+    public static void main(String[] args) {
+        List<List<Integer>> temp = new ArrayList<>();
+        List<Integer> a1 = new ArrayList<>();
+    }
+
+
+    public static int minimumTotal(List<List<Integer>> triangle) {
+        // 特判
+        if (triangle == null || triangle.size() == 0) {
+            return 0;
+        }
+        int[][] dp = new int[triangle.size() + 1][triangle.size() + 1];
+        for (int i = triangle.size() - 1; i >= 0; i--) {
+            List<Integer> rows = triangle.get(i);
+            for (int j = 0; j < rows.size(); j++) {
+                dp[i][j] = Math.min(dp[i + 1][j], dp[i + 1][j + 1]) + rows.get(j);
+            }
+        }
+        return dp[0][0];
+    }
+
+}
