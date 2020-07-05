75f08a0d9237577fc536836e32811391e0c9d54b
hmj <hu782980856@gmail.com>
2020-07-05 17:14:28 +0800

    二叉树的最近公共最先

--- src/LeetCode/BinTree/lowestCommonAncestor.java
+++ src/LeetCode/BinTree/lowestCommonAncestor.java
@@ -41,6 +41,13 @@ public class lowestCommonAncestor {
         return null;
     }
 
+    /**
+     * 二叉搜索树的最近公共祖先
+     * @param root
+     * @param p
+     * @param q
+     * @return
+     */
     public TreeNode lowestCommonAncestor2(TreeNode root, TreeNode p, TreeNode q) {
         if (p == root) {
             return p;
@@ -54,5 +61,31 @@ public class lowestCommonAncestor {
             return lowestCommonAncestor2(root.left, p, q);
         }
         return root;
+    }
+
+    /**
+     * 二叉树的最近公共最先
+     * @param root
+     * @param p
+     * @param q
+     * @return
+     */
+    public TreeNode lowestCommonAncestor3(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return root;
+        }
+        if (p == root || q == root) {
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor3(root.left, p, q);
+        TreeNode right = lowestCommonAncestor3(root.right, p, q);
+        if (left != null && right != null) {
+            return root;
+        } else if (left != null) {
+            return left;
+        } else if (right != null) {
+            return right;
+        }
+        return null;
     }
 }
