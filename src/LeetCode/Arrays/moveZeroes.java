413669733ad30d308942d18dde2e5d2cc8638f7c
hmj <hu782980856@gmail.com>
2020-07-05 15:45:10 +0800

    把所有的0移动到数组的末尾，且不改变非零元素的相对位置

---
+++ src/LeetCode/Arrays/moveZeroes.java
@@ -0,0 +1,29 @@
+package LeetCode.Arrays;
+
+/**
+ * @author humingjiang 2020-07-05 15:13
+ **/
+public class moveZeroes {
+
+    public static void main(String[] args) {
+        moveZeroes(new int[]{0, 1, 0, 3, 12});
+    }
+    /**
+     * 移动零
+     * @param nums
+     */
+    public static void moveZeroes(int[] nums) {
+        if (nums == null || nums.length < 1) {
+            return;
+        }
+        int nonzero = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[nonzero++] = nums[i];
+            }
+        }
+        for (int i = nonzero; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+    }
+}
