5132f8e770bcd0ad324e16cd77cec6b622399577
hmj <hu782980856@gmail.com>
2020-07-11 10:22:04 +0800

    fix

--- src/LeetCode/BinTree/lowestCommonAncestor.java
+++ src/LeetCode/BinTree/lowestCommonAncestor.java
@@ -22,32 +22,6 @@ public class lowestCommonAncestor {
      * @param q
      * @return
      */
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if (root == null) {
-            return null;
-        }
-        if (p == root || q == root) {
-            return root;
-        }
-        TreeNode left = lowestCommonAncestor(root.left, p, q);
-        TreeNode right = lowestCommonAncestor(root.right, p, q);
-        if (left != null && right != null) {
-            return root;
-        } else if (left != null) {
-            return left;
-        } else if (right != null) {
-            return right;
-        }
-        return null;
-    }
-
-    /**
-     * 二叉搜索树的最近公共祖先
-     * @param root
-     * @param p
-     * @param q
-     * @return
-     */
     public TreeNode lowestCommonAncestor2(TreeNode root, TreeNode p, TreeNode q) {
         if (p == root) {
             return p;
