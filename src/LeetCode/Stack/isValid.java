e5a1c7387bb9e28fcd3d0bd4a30e7ada6e3c3ba4
hmj <hu782980856@gmail.com>
2020-07-11 14:25:17 +0800

    括号匹配，使用栈的数据结构来完成

---
+++ src/LeetCode/Stack/isValid.java
@@ -0,0 +1,56 @@
+package LeetCode.Stack;
+
+import java.util.Stack;
+
+/**
+ * @author humingjiang 2020-07-11 14:17
+ **/
+public class isValid {
+
+    /**
+     * 是否是有效的括号
+     * @param s
+     * @return
+     */
+    public boolean isValid(String s) {
+        if (s == null || s.length() < 1) {
+            return true;
+        }
+        Stack<Character> stack = new Stack<>();
+        char[] characters = s.toCharArray();
+        for (int i = 0; i < characters.length; i++) {
+            if (characters[i] == '{' || characters[i] == '[' || characters[i] == '(') {
+                stack.push(characters[i]);
+            }
+            if (stack.isEmpty()) {
+                return false;
+            }else {
+                if (characters[i] == '}') {
+                    char top = stack.peek();
+                    if (top == '{') {
+                        stack.pop();
+                    }else {
+                        return false;
+                    }
+                }
+                if (characters[i] == ']') {
+                    char top = stack.peek();
+                    if (top == '[') {
+                        stack.pop();
+                    }else {
+                        return false;
+                    }
+                }
+                if (characters[i] == ')') {
+                    char top = stack.peek();
+                    if (top == '(') {
+                        stack.pop();
+                    }else {
+                        return false;
+                    }
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
