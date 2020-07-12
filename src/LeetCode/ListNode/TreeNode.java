1d3f3cfc0899f41cc0f65210f6d502f166821578
hmj <hu782980856@gmail.com>
2020-07-12 16:53:04 +0800

    有序单链表转换为二叉搜索树，思路跟有序数组转换为二叉搜索树一样的，只是单链表的中点的寻找是要快慢指针来做

---
+++ src/LeetCode/ListNode/TreeNode.java
@@ -0,0 +1,14 @@
+package LeetCode.ListNode;
+
+/**
+ * @author humingjiang 2020-07-12 16:38
+ **/
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
---
+++ src/LeetCode/ListNode/sortedListToBST.java
@@ -0,0 +1,35 @@
+package LeetCode.ListNode;
+
+/**
+ * @author humingjiang 2020-07-12 16:38
+ **/
+public class sortedListToBST {
+
+    /**
+     * 把一个有序的单链表转换为二叉搜索树
+     * @param head
+     * @return
+     */
+    public TreeNode sortedListToBST(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        if (head.next == null) {
+            return new TreeNode(head.val);
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode pre = head;
+        while (fast != null && fast.next != null) {
+            pre = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        // slow指向的就是中点节点
+        pre.next = null;
+        TreeNode root = new TreeNode(slow.val);
+        root.left = sortedListToBST(head);
+        root.right = sortedListToBST(slow.next);
+        return root;
+    }
+}
