83cfaf6c427f1213b1e61bbc340f26753203e04d
hmj <hu782980856@gmail.com>
2020-07-19 11:05:24 +0800

    重建二叉树，注意保持中序数组和前序数组的个数一直就不会错了

---
+++ src/JianZhiOffer/TreeNode.java
@@ -0,0 +1,14 @@
+package JianZhiOffer;
+
+/**
+ * @author humingjiang 2020-07-19 10:59
+ **/
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
---
+++ src/JianZhiOffer/buildTree.java
@@ -0,0 +1,30 @@
+package JianZhiOffer;
+
+import java.util.Arrays;
+
+/**
+ * @author humingjiang 2020-07-19 10:59
+ **/
+public class buildTree {
+
+    /**
+     * 根据前序和中序重建二叉树
+     * @param preorder
+     * @param inorder
+     * @return
+     */
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        if (preorder == null || inorder == null || preorder.length < 1 || inorder.length < 1) {
+            return null;
+        }
+        TreeNode root = new TreeNode(preorder[0]);
+        for (int i = 0; i < inorder.length; i++) {
+            if (preorder[0] == inorder[i]) {
+                root.left = buildTree(Arrays.copyOfRange(preorder, 1, i+1), Arrays.copyOfRange(inorder, 0, i));
+                root.right = buildTree(Arrays.copyOfRange(preorder, i+1, preorder.length), Arrays.copyOfRange(inorder, i + 1, inorder.length));
+                break;
+            }
+        }
+        return root;
+    }
+}
