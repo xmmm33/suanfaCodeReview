42aeb84e922068b2a6d0be585c084d4b9b543e2a
hmj <782980856@qq.com>
2020-07-19 16:51:45 +0800

    判断是不是子树，注意复习

---
+++ src/JianZhiOffer/isSubStructure.java
@@ -0,0 +1,30 @@
+package JianZhiOffer;
+
+/**
+ * @author humingjiang 2020-07-19 16:44
+ **/
+public class isSubStructure {
+
+    /**
+     * 判定B是不是A的子树
+     * @param A
+     * @param B
+     * @return
+     */
+    public boolean isSubStructure(TreeNode A, TreeNode B) {
+        if (A == null || B == null) {
+            return false;
+        }
+        return dfs(A, B) || isSubStructure(A.left, B) || isSubStructure(A.right, B);
+    }
+
+    private boolean dfs(TreeNode a, TreeNode b) {
+        if (b == null) {
+            return true;
+        }
+        if (a == null) {
+            return false;
+        }
+        return a.val == b.val && dfs(a.left, b.left) && dfs(a.right, b.right);
+    }
+}
