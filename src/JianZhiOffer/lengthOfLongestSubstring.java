db4cc54ab547c3fe9d35e24f5737ff7eb1e23752
humingjiang <humingjiang@meituan.com>
2020-07-25 15:25:51 +0800

    最长无重复字符的字串的长度,滑动窗口的思想

---
+++ src/JianZhiOffer/lengthOfLongestSubstring.java
@@ -0,0 +1,38 @@
+package JianZhiOffer;
+
+import org.omg.PortableInterceptor.INACTIVE;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author humingjiang 2020-07-25 15:18
+ **/
+public class lengthOfLongestSubstring {
+
+    /**
+     * 最长不重复的字串
+     * @param s
+     * @return
+     */
+    public int lengthOfLongestSubstring(String s) {
+        if (s == null || s.length() < 1) {
+            return 0;
+        }
+        int max = 0;
+        Map<Character, Integer> index = new HashMap<>();
+        char[] chars = s.toCharArray();
+        int left = 0;
+        int right = 0;
+        while (right < chars.length) {
+            if (index.containsKey(chars[right])) {
+                // 更新滑动窗口左边界
+                left = Math.max(left, index.get(chars[right])+1);
+            }
+            max = Math.max(max, right - left + 1);
+            index.put(chars[right], right);
+            right++;
+        }
+        return max;
+    }
+}
