610ad7045dd9fe0461c1f0bb178cd0e5433920eb
humingjiang <humingjiang@meituan.com>
2020-07-25 11:02:07 +0800

    复杂链表的复制

---
+++ src/JianZhiOffer/copyRandomList.java
@@ -0,0 +1,54 @@
+package JianZhiOffer;
+
+import java.util.List;
+
+/**
+ * @author humingjiang 2020-07-25 10:49
+ **/
+public class copyRandomList {
+
+    class Node {
+        int val;
+        Node next;
+        Node random;
+
+        public Node(int val) {
+            this.val = val;
+            this.next = null;
+            this.random = null;
+        }
+    }
+
+    public Node copyRandomList(Node head) {
+        if (head == null) {
+            return head;
+        }
+        Node temp = head;
+        while (temp != null) {
+            Node next = temp.next;
+            Node copy = new Node(temp.val);
+            temp.next = copy;
+            copy.next = next;
+            temp = next;
+        }
+        temp = head;
+        while (temp != null && temp.next != null) {
+            if (temp.random != null) {
+                temp.next.random = temp.random.next;
+            }
+            temp = temp.next.next;
+        }
+        Node h = head.next;
+        Node newhead = h;
+        temp = head;
+        while (temp != null) {
+            temp.next = temp.next.next;
+            temp = temp.next;
+            if (h.next != null) {
+                h.next = h.next.next;
+                h = h.next;
+            }
+        }
+        return newhead;
+    }
+}
