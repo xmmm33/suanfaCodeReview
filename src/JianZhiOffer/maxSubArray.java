c4123dc66f765474f27b61bd843c8621a376b1cd
humingjiang <humingjiang@meituan.com>
2020-07-25 14:26:22 +0800

    最大的连续子数组和

---
+++ src/JianZhiOffer/maxSubArray.java
@@ -0,0 +1,26 @@
+package JianZhiOffer;
+
+/**
+ * @author humingjiang 2020-07-25 14:09
+ **/
+public class maxSubArray {
+
+    /**
+     * 连续子数组最大和
+     * @param nums
+     * @return
+     */
+    public int maxSubArray(int[] nums) {
+        if (nums == null || nums.length < 1) {
+            return 0;
+        }
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        int max =0;
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
+            max = Math.max(dp[i], max);
+        }
+        return max;
+    }
+}
