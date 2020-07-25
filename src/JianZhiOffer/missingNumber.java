1cfba902d803c943664a123c592a5611dbcbfae8
humingjiang <humingjiang@meituan.com>
2020-07-25 16:42:46 +0800

    排序数组汇总缺失的数字

---
+++ src/JianZhiOffer/missingNumber.java
@@ -0,0 +1,28 @@
+package JianZhiOffer;
+
+/**
+ * @author humingjiang 2020-07-25 16:37
+ **/
+public class missingNumber {
+
+    /**
+     * 排序数组中缺失的数字
+     * @param nums
+     * @return
+     */
+    public int missingNumber(int[] nums) {
+        int low = 0;
+        int high = nums.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == mid) {
+                //如果相等，说明前面没有缺失，要往后面找
+                low = mid + 1;
+            }else {
+                //如果不相等，说明缺失的在前面，要往前面找
+                high = mid - 1;
+            }
+        }
+        return low;
+    }
+}
