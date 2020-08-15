09dd292fa40841b2164e793f252a9379ff843e21
humingjiang <humingjiang@meituan.com>
2020-08-15 18:38:14 +0800

    插入排序和希尔排序练习

--- src/insertSort/insertSortPratice.java
+++ src/insertSort/insertSortPratice.java
@@ -3,10 +3,28 @@ package insertSort;
 public class insertSortPratice {
 
     public static void main(String[] args) {
-        inserSortPractice(new int[]{5, 34, 56, 6, 5, 6, 1, 24, 4});
+        insert(new int[]{5, 34, 56, 6, 5, 6, 1, 24, 4});
     }
 
     public static void inserSortPractice(int[] arrays) {
+        if (arrays == null || arrays.length < 1) {
+            return;
+        }
+        for (int i = 1; i < arrays.length; i++) {
+            for (int j = i; j > 0; j--) {
+                if (arrays[j] < arrays[j - 1]) {
+                    int temp = arrays[j];
+                    arrays[j] = arrays[j - 1];
+                    arrays[j - 1] = temp;
+                }
+            }
+        }
+        for (Integer i : arrays) {
+            System.out.println(i);
+        }
+    }
+
+    public static void insert(int[] arrays) {
         if (arrays == null || arrays.length < 1) {
             return;
         }
