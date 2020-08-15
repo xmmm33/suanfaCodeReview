b5c0cf9fb8e54e4a6e861101015d98c2757d0d65
xmmm <782980856@qq.com>
2020-08-15 20:14:24 +0800

    归并排序练习

--- src/guiBingSort/guiBingPratice.java
+++ src/guiBingSort/guiBingPratice.java
@@ -3,7 +3,7 @@ package guiBingSort;
 public class guiBingPratice {
 
     public static void main(String[] args) {
-        guiBingPratice(new int[]{5, 34, 56, 6, 5, 6, 1, 24, 4});
+        gb(new int[]{5, 34, 56, 6, 5, 6, 1, 24, 4});
     }
 
     public static void guiBingPratice(int[] arrays) {
@@ -46,5 +46,46 @@ public class guiBingPratice {
         for (int i = left; i <= right; i++) {
             arrays[i] = fuzhi[i];
         }
+    }
+
+    public static void gb(int[] arrays) {
+        if (arrays == null || arrays.length < 1) {
+            return;
+        }
+        partly(arrays, 0, arrays.length - 1);
+        for (Integer i : arrays) {
+            System.out.println(i);
+        }
+    }
+
+    private static void partly(int[] arrays, int left, int right) {
+        if (left >= right) {
+            return;
+        }
+        int mid = left + (right - left) / 2;
+        partly(arrays, left, mid);
+        partly(arrays, mid + 1, right);
+        mergrmy(arrays, left, mid, right);
+    }
+
+    private static void mergrmy(int[] arrays, int left, int mid, int right) {
+        int low = left;
+        int k = left;
+        int high = mid + 1;
+        int[] fz = new int[arrays.length];
+        while (low <= mid && high <= right) {
+            if (arrays[low] < arrays[high]) {
+                fz[k++] = arrays[low++];
+            }else {
+                fz[k++] = arrays[high++];
+            }
+        }
+        while (low <= mid) {
+            fz[k++] = arrays[low++];
+        }
+        while (high <= right) {
+            fz[k++] = arrays[high++];
+        }
+        if (right + 1 - left >= 0) System.arraycopy(fz, left, arrays, left, right + 1 - left);
     }
 }
