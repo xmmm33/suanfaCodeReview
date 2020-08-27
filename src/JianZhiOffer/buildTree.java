c2e91c1ee312199d49d742450c4ccbfb8ac8bc6d
xmmm <782980856@qq.com>
2020-08-27 12:06:26 +0800

    fix

--- src/JianZhiOffer/buildTree.java
+++ src/JianZhiOffer/buildTree.java
@@ -20,8 +20,8 @@ public class buildTree {
         TreeNode root = new TreeNode(preorder[0]);
         for (int i = 0; i < inorder.length; i++) {
             if (preorder[0] == inorder[i]) {
-                root.left = buildTree(Arrays.copyOfRange(preorder, 1, i+1), Arrays.copyOfRange(inorder, 0, i));
-                root.right = buildTree(Arrays.copyOfRange(preorder, i+1, preorder.length), Arrays.copyOfRange(inorder, i + 1, inorder.length));
+                root.left = buildTree(Arrays.copyOfRange(preorder, 1, i + 1), Arrays.copyOfRange(inorder, 0, i));
+                root.right = buildTree(Arrays.copyOfRange(preorder, i + 1, preorder.length), Arrays.copyOfRange(inorder, i + 1, inorder.length));
                 break;
             }
         }
