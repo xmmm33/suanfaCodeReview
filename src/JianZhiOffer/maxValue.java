97d09f8e561971d4f6c592c763a8df02dc955283
xmmm <782980856@qq.com>
2020-09-08 13:48:14 +0800

    从矩阵的左上角走到右下角的最大值路径练习

--- src/JianZhiOffer/maxValue.java
+++ src/JianZhiOffer/maxValue.java
@@ -8,8 +8,10 @@ public class maxValue {
     public static void main(String[] args) {
         maxValue(new int[][]{{1, 2, 5}, {3, 2, 1}});
     }
+
     /**
      * 最大的路径
+     *
      * @param grid
      * @return
      */
@@ -22,6 +24,28 @@ public class maxValue {
         for (int i = 1; i < grid.length; i++) {
             dp[i][0] = dp[i - 1][0] + grid[i][0];
         }
+        for (int i = 1; i < grid[0].length; i++) {
+            dp[0][i] = dp[0][i - 1] + grid[0][i];
+        }
+        for (int i = 1; i < grid.length; i++) {
+            for (int j = 1; j < grid[0].length; j++) {
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+            }
+        }
+        return dp[grid.length - 1][grid[0].length - 1];
+    }
+
+    public static int maxValue2(int[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) {
+            return 0;
+        }
+        int[][] dp = new int[grid.length][grid[0].length];
+        dp[0][0] = grid[0][0];
+        // 初始化列
+        for (int i = 1; i < grid.length; i++) {
+            dp[i][0] = dp[i - 1][0] + grid[i][0];
+        }
+        // 初始化行
         for (int i = 1; i < grid[0].length; i++) {
             dp[0][i] = dp[0][i - 1] + grid[0][i];
         }
