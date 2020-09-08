1a8104c3ae27e889dbc2d6133b4bf997a8959572
xmmm <782980856@qq.com>
2020-09-08 14:14:13 +0800

    找到一个排序数组中缺数的数字，使用二分法定位，äãd]å¸比较mid，练习

--- src/JianZhiOffer/missingNumber.java
+++ src/JianZhiOffer/missingNumber.java
@@ -7,6 +7,7 @@ public class missingNumber {
 
     /**
      * 排序数组中缺失的数字
+     *
      * @param nums
      * @return
      */
@@ -18,8 +19,25 @@ public class missingNumber {
             if (nums[mid] == mid) {
                 //如果相等，说明前面没有缺失，要往后面找
                 low = mid + 1;
-            }else {
+            } else {
                 //如果不相等，说明缺失的在前面，要往前面找
+                high = mid - 1;
+            }
+        }
+        return low;
+    }
+
+    public int missingNumber2(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int low = 0;
+        int high = nums.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == mid) {
+                low = mid + 1;
+            }else {
                 high = mid - 1;
             }
         }
