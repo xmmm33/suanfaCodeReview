51480d93411454a20f011bcfe22a5ed718f2d155
xmmm <782980856@qq.com>
2020-09-08 14:09:54 +0800

    生成二叉树的镜像，先序偏遍历既åå即可

--- src/JianZhiOffer/mirrorTree.java
+++ src/JianZhiOffer/mirrorTree.java
@@ -7,6 +7,7 @@ public class mirrorTree {
 
     /**
      * 二叉树的镜像
+     *
      * @param root
      * @return
      */
@@ -19,6 +20,19 @@ public class mirrorTree {
         root.right = temp;
         mirrorTree(root.left);
         mirrorTree(root.right);
+        return root;
+    }
+
+    public TreeNode mirrorTree2(TreeNode root) {
+        if (root == null) {
+            return root;
+        }
+        // 交换左右子树
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        mirrorTree2(root.left);
+        mirrorTree2(root.right);
         return root;
     }
 }
