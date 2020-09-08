379f6edff57ff4094726f9b33ae3721689c5c511
xmmm33 <782980856@qq.com>
2020-09-08 15:06:57 +0800

    找å®n个æ丑数练习

--- src/JianZhiOffer/nthUglyNumber.java
+++ src/JianZhiOffer/nthUglyNumber.java
@@ -10,6 +10,7 @@ public class nthUglyNumber {
 
     /**
      * 求第n个丑数 只包含2 3 5因子的数
+     *
      * @param n
      * @return
      */
@@ -34,6 +35,31 @@ public class nthUglyNumber {
             }
             temp.add(a);
         }
-        return temp.get(n-1);
+        return temp.get(n - 1);
+    }
+
+    public int nthUglyNumber2(int n) {
+        int two =0;
+        int three = 0;
+        int five = 0;
+        List<Integer> ret = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            if (i == 1) {
+                ret.add(1);
+                continue;
+            }
+            int cur = Math.min(ret.get(two) * 2, Math.min(ret.get(three) * 3, ret.get(five) * 5));
+            if (ret.get(two) * 2 == cur) {
+                two++;
+            }
+            if (ret.get(three) * 3 == cur) {
+                three++;
+            }
+            if (ret.get(five) * 5 == cur) {
+                five++;
+            }
+            ret.add(cur);
+        }
+        return ret.get(n - 1);
     }
 }
