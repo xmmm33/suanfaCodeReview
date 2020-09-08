a432d13422f9d5474e267753136dd8f1fb3ec231
xmmm <782980856@qq.com>
2020-09-08 13:40:32 +0800

    连续子数组最大和练习

--- src/JianZhiOffer/maxSubArray.java
+++ src/JianZhiOffer/maxSubArray.java
@@ -7,6 +7,7 @@ public class maxSubArray {
 
     /**
      * 连续子数组最大和
+     *
      * @param nums
      * @return
      */
@@ -16,11 +17,26 @@ public class maxSubArray {
         }
         int[] dp = new int[nums.length];
         dp[0] = nums[0];
-        int max =0;
+        int max = 0;
         for (int i = 1; i < nums.length; i++) {
             dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
             max = Math.max(dp[i], max);
         }
         return max;
     }
+
+    public int maxSubArray2(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        int max = Integer.MIN_VALUE;
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
+            max = Math.max(max, nums[i]);
+        }
+        return max;
+    }
+
 }
