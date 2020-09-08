f735ebdb6ad5b88cf10e6931984f8263ddf522b0
xmmm <782980856@qq.com>
2020-09-08 18:50:11 +0800

    小米笔试

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,34 @@
 import java.util.*;
 
 public class Main{
+    public static final String num = ".*\\d+.*";
+
+    public static final String daxie = ".*[A-Z]+.*";
+
+    public static final String xiaoxie = ".*[a-z]+.*";
+
+    public static final String dot = ".*[~!@#$%^&*()_+|<>,.?/:;'\\[\\]{}\"]+.*";
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int len = sc.nextInt();
-        String target = sc.nextLine();
-        target = sc.nextLine();
-        int max = 1;
-        for (int i = len / 2; i >= 0; i--) {
-            if (target.lastIndexOf(target.substring(0,i)) > 0) {
-                max = Math.max(max, i);
-                break;
-            }
+        String inputstring = sc.nextLine();
+        String[] passwords = inputstring.split(" ");
+        for (int i = 0; i < passwords.length; i++) {
+            System.out.println(checkIsCorrect(passwords[i]));;
         }
-        System.out.println(len - max + 1);
+    }
+
+    private static int checkIsCorrect(String password) {
+        if (password.length() < 8 || password.length() > 120) {
+            return 1;
+        }
+        int i = 0;
+        if (password.matches(num)) i++;
+        if (password.matches(daxie)) i++;
+        if (password.matches(xiaoxie)) i++;
+        if (password.matches(dot)) i++;
+        if (i < 4) {
+            return 2;
+        }
+        return 0;
     }
 }
