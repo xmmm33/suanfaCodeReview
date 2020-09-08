43171f844197f63a345caacc1a286c3f4b8bf506
xmmm33 <782980856@qq.com>
2020-09-08 15:42:36 +0800

    字符串的全排列

--- src/JianZhiOffer/permutation.java
+++ src/JianZhiOffer/permutation.java
@@ -1,6 +1,8 @@
 package JianZhiOffer;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -9,8 +11,10 @@ import java.util.Set;
 public class permutation {
 
     Set<String> temp = new HashSet<>();
+
     /**
      * 字符串的全排列
+     *
      * @param s
      * @return
      */
@@ -38,6 +42,35 @@ public class permutation {
             dfs(chars, strs, vis);
             vis[i] = false;
             strs.deleteCharAt(strs.length() - 1);
+        }
+    }
+
+    List<String> result = new ArrayList<>();
+
+    public String[] permutation2(String s) {
+        if (s == null || s.length() == 0) {
+            return new String[0];
+        }
+        boolean[] vis = new boolean[s.length()];
+        char[] chars = s.toCharArray();
+        dfs2(chars, new StringBuilder(), vis);
+        return result.toArray(new String[0]);
+    }
+
+    private void dfs2(char[] chars, StringBuilder stringBuilder, boolean[] vis) {
+        if (stringBuilder.length() == chars.length) {
+            result.add(new String(stringBuilder));
+            return;
+        }
+        for (int i = 0; i < chars.length; i++) {
+            if (vis[i]) {
+                continue;
+            }
+            vis[i] = true;
+            stringBuilder.append(chars[i]);
+            dfs(chars, stringBuilder, vis);
+            vis[i] = false;
+            stringBuilder.deleteCharAt(stringBuilder.length() - 1);
         }
     }
 }
